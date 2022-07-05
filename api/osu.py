e1fccc533c356f82bf90b887a71cdb719a7393ba
Tiller <tylere431@gmail.com>
2022-07-05 03:20:31 -0500

    add getOsu func

--- api/osu.py
+++ api/osu.py
@@ -1,16 +1,17 @@
+from linecache import cache
 import requests
 import json
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+cachePath = "cache/osu/"
 apiKey = os.getenv("OSU_API_KEY")
 
 def getBeatmap(beatmapID):
     url = "https://osu.ppy.sh/api/get_beatmaps?k=" + apiKey + "&b=" + beatmapID
     response = requests.get(url)
-    response = response.json()
+    response = response.json()[0]
     return response
 
 def getUser(userID):
@@ -26,3 +27,14 @@ def getUserTop(userID, limit):
     return response
 
 
+def getOsu(beatmapID):
+    #cache/osu/beatmapID.osu
+    beatmapID = str(beatmapID)
+    url = "https://osu.ppy.sh/osu/{}".format(beatmapID)
+    response = requests.get(url)
+    if os.path.exists(cachePath + beatmapID + ".osu"):
+        os.remove(cachePath + beatmapID + ".osu")
+    
+    with open(cachePath + beatmapID + ".osu", "w") as f:
+        f.write(response.text)
+    return True
